24e929b60439441f34f750b4e4a057097ca9897f
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:03:47 +0530

    Delete practice20.py

--- practice20.py
+++
@@ -1,9 +0,0 @@
-#using function convert degree celsius to farhenheit
-#temp=float(input("enter temp in celcius:"))
-def convert(f):
-    f=(temp*1.8)+32
-    return f
-temp=float(input("enter the temp:"))
-result=convert(temp)
-print("temp in farhenheit is:",result)
-
