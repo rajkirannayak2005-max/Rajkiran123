877449d967e5fb10d778dea7f716fcdc4819ed78
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:06:15 +0530

    Delete ppml exp=7.8.py

--- ppml exp=7.8.py
+++
@@ -1,7 +0,0 @@
-#write a function to calculate simple interest
-p=float(input("enter principal:"))
-r=float(input("enter rate:"))
-t=float(input("enter time in year:"))
-def simple_interest(p,r,t):
-    return (p*t*r)/100
-print("simple interest is :", simple_interest(p,t,r))
