d14a9be7dc121bb8581d5c52eb08af5622b6ca89
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:00:51 +0530

    Delete ppml exp-7.3.py

--- ppml exp-7.3.py
+++
@@ -1,9 +0,0 @@
-#write a function to find the factorial of a number
-n=int(input("enter a number:"))
-def factorial(n):
-    if n==0 or n==1:
-        return 1
-    else:
-        return n* factorial(n-1)
-result=factorial(n) 
-print("factorial of number is:",result)     
