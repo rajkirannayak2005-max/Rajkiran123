0cafd3ab02ae8b84fa4aab2a0857b8b355202f05
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:00:22 +0530

    Delete ppml exp-7.1.py

--- ppml exp-7.1.py
+++
@@ -1,7 +0,0 @@
-#write a program to find the square of a number using function
-def square(n):
-    s=n*n
-    return s
-number=int(input("enter the number:"))
-result=square(number)
-print("square of number is:",result)
