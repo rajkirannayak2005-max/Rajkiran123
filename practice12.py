02b7576e6b5cf78e23b55e75c9a8d52313aa411e
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:07:23 +0530

    Delete practice12.py

--- practice12.py
+++
@@ -1,8 +0,0 @@
-#write a program to fill a letter templet below with name and date
-name=input("enter your name:")
-date=input("enter date:")
-letter='''Dear {name},
-                 you are selected!
-                 {date}'''
-
-print(letter.format(date=date , name=name))
