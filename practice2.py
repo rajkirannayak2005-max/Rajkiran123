bd5a21f7275726b22f311b4d5e53e5406eb046ed
rajkirannayak2005-max <rajkirannayak2005@gmail.com>
2026-02-25 17:03:32 +0530

    Delete practice2.py

--- practice2.py
+++
@@ -1,8 +0,0 @@
-#write a program to find remainder when a number is divedide by Z and print type of variable assinged
-n=int(input("enter a number:"))
-z=int(input("enter the divider:"))
-d=n%z
-print("the remainder is:",d)
-print(type(n))
-print(type(z))
-print(type(d))
